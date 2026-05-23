39ef5683bbc507a2f1ee2095c71a05233d2a6d4e
Shorena <froztyk321@gmail.com>
2026-05-23 14:16:30 +0400

    simplify stars to channel link

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ bot.command('start', async (ctx) => {
         parse_mode: 'HTML',
         reply_markup: {
           inline_keyboard: [
-            [{ text: '💎 Buy for ' + PRICE + ' TON', url: tonkeeperLink }],
+            [{ text: '💎 Buy for ' + PRICE + ' TON ~46$', url: tonkeeperLink }],
             [{ text: '⭐ Buy for ' + STARS_PRICE + ' Stars', callback_data: 'buy_stars' }],
             [{ text: '✅ I paid (TON)', callback_data: 'verify' }],
             [{ text: '👀 See previews', callback_data: 'preview' }]
@@ -97,7 +97,7 @@ bot.action('buy_stars', async (ctx) => {
         parse_mode: 'HTML',
         reply_markup: {
           inline_keyboard: [
-            [{ text: '⭐ Join for ' + STARS_PRICE + ' Stars', url: 'https://t.me/+1sGbJzUH4g00NjZh' }]
+            [{ text: '⭐ Join for ' + STARS_PRICE + ' Stars ~60$', url: 'https://t.me/+1sGbJzUH4g00NjZh' }]
           ]
         }
       }
